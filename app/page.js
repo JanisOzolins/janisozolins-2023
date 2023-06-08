6558271bb1ccf463d90442f4f36ea890b5e0ca02
JanisOzolins <janis@janisozolins.com>
2023-06-08 23:47:48 +0300

    Add hero banner

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
+import Hero from '@/app/components/Hero/hero'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      test
+      <Hero></Hero>
     </main>
   )
 }
