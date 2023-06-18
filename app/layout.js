7dbaff425c799564786f9cf5eaa6115c0ffb0b82
JanisOzolins <janis@janisozolins.com>
2023-06-18 17:31:08 +0200

    Change theme to black

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="theme-color" content="#212121" />
+      <meta name="theme-color" content="#000000" />
       <body className={montserrat.className}>{children}</body>
     </html>
   );
