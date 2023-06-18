494789667c59bc2a6f1dd019f8837eb0a0a51667
JanisOzolins <janis@janisozolins.com>
2023-06-18 16:37:33 +0200

    Favicon and theme

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="theme-color" content="#4285f4" />
       <body className={montserrat.className}>{children}</body>
     </html>
   );
